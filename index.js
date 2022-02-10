a27b0587126734deb26ce9260e8928bbcb1bc7ba
Muhammad Ali Musthofa <muhammadalimusthofa786@gmail.com>
2022-02-10 15:42:41 +0700

    day 3 cp.2 ini bos

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ app.use(express.urlencoded({ extended: false }));
 // false => belum login
 const isLogin = true;
 
+//Membuat Array Object yg akan menyimpan Data blog list
+const blogs = [
+  {
+    title: 'Judul',
+    content: 'Judul',
+    author: 'Judul',
+    posted_ad: 'Judul',
+  },
+];
+//bulan
+let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'Desember'];
+
 // Set endpoint
 app.get('/', function (req, res) {
   res.send('Hello World');
@@ -23,20 +35,49 @@ app.get('/home', function (req, res) {
   res.render('index');
 });
 
+//memanipulasi data atau menambahkan data ke list blog apabila ada blog baru yg diinput
 app.get('/blog', function (req, res) {
-  res.render('blog', { isLogin: isLogin });
+  console.log(blogs);
+
+  let dataBlogs = blogs.map(function (data) {
+    return {
+      ...data,
+      isLogin: isLogin,
+    };
+  });
+
+  res.render('blog', { isLogin: isLogin, blogs: dataBlogs });
 });
 
+//add
 app.get('/add-blog', function (req, res) {
   res.render('form-blog');
 });
 
+//DELETE list blog
+app.get('/delete-blog/:index', function (req, res) {
+  let index = req.params.index;
+
+  console.log(`Index Data : ${index}`);
+
+  blogs.splice(index, 1);
+  res.redirect('/blog');
+});
+
+//mengambil data dari inputan blog agar bisa di tambahkan di list blog
 app.post('/blog', function (req, res) {
   let title = req.body.title;
   let content = req.body.content;
 
-  console.log(`Data title : ${title}`);
-  console.log(`Data content : ${content}`);
+  let blog = {
+    title: title,
+    content,
+    author: 'Ali Musthofa',
+    posted_at: getFullTime(new Date()),
+  };
+
+  blogs.push(blog);
+  res.redirect('/blog');
 });
 
 app.get('/blog/:id', function (req, res) {
@@ -55,3 +96,14 @@ const port = 3000;
 app.listen(port, function () {
   console.log(`server running on port ${port}`);
 });
+
+function getFullTime(time) {
+  let date = time.getDate();
+  let monthIndex = time.getMonth();
+  let year = time.getFullYear();
+
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  return ` ${date} ${month[monthIndex]}  ${year} ${hours}:${minutes} WIB`;
+}
