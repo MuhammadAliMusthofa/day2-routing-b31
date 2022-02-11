2ee70dae7b4632e7799022d3de50845e5020d60e
Muhammad Ali Musthofa <muhammadalimusthofa786@gmail.com>
2022-02-11 16:01:02 +0700

    materi day 4

---
+++ connection/db.js
@@ -0,0 +1,13 @@
+// import postgres pool
+const { Pool } = require('pg');
+
+//setup connection pool
+const dbPool = new Pool({
+  database: 'ini database', //nama databaseku
+  port: 5432,
+  user: 'nama user', //liat di postgree yg kita buat tabenya
+  password: '', //password yg kita masukin saat pertama kali install pg
+});
+
+//export db pool
+module.exports = dbPool;
